82bbe902754b34fb20a69d8c18a4f8f3cc3f0a21
Alexander Johansson <alexander@n1s.se>
2025-01-23 12:24:37 +0100

    mkay

--- src/client.ts
+++ src/client.ts
@@ -13,40 +13,35 @@ const client = createTRPCClient<AppRouter>({
   ],
 });
 
-const messages: Inputs['chat']['messages'] = [];
+const chat = (function () {
+  const messages: Inputs['chat']['messages'] = [];
 
-{
-  messages.push({ role: 'user', content: 'What is the capital of France?' });
+  return {
+    async ask(question: string) {
+      process.stdout.write(`Asking: ${question}\n`);
+      messages.push({ role: 'user', content: question });
 
-  const res = await client.chat.mutate({
-    messages,
-  });
+      process.stdout.write('... ');
+      const res = await client.chat.mutate({
+        messages,
+      });
+      process.stdout.write('response:\n');
 
-  let allChunks: string[] = [];
-  for await (const chunk of res) {
-    process.stdout.write(chunk);
-    allChunks.push(chunk);
-  }
-  process.stdout.write('\n\n');
+      let allChunks: string[] = [];
 
-  messages.push({ role: 'assistant', content: allChunks.join('') });
-}
+      for await (const chunk of res) {
+        process.stdout.write(chunk);
+        allChunks.push(chunk);
+      }
 
-{
-  messages.push({ role: 'user', content: 'What is the capital of the UK?' });
+      process.stdout.write('\n\n');
 
-  const res = await client.chat.mutate({
-    messages,
-  });
+      messages.push({ role: 'assistant', content: allChunks.join('') });
 
-  let allChunks: string[] = [];
-  for await (const chunk of res) {
-    process.stdout.write(chunk);
-    allChunks.push(chunk);
-  }
+      return allChunks.join('');
+    },
+  };
+})();
 
-  process.stdout.write('\n\n');
-  messages.push({ role: 'assistant', content: allChunks.join('') });
-}
-
-console.log({ messages });
+await chat.ask('What is the capital of France?');
+await chat.ask('What is the capital of the UK?');
