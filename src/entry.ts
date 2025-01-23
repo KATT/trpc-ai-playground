de916bbbfc7ff60bced805d9f9b47f7bb6033d6a
Alexander Johansson <alexander@n1s.se>
2025-01-23 11:43:15 +0100

    add ai

--- src/entry.ts
+++ src/entry.ts
@@ -1,3 +1,18 @@
-await new Promise<void>(resolve => setTimeout(resolve, 1000));
+import { anthropic } from '@ai-sdk/anthropic';
+import { streamText } from 'ai';
 
-console.log('Hello, world!');
+const model = anthropic('claude-3-5-haiku-latest');
+
+async function answerMyQuestion(opts: { prompt: string }) {
+  const response = streamText({
+    model,
+    prompt: opts.prompt,
+  });
+  return response;
+}
+
+const res = await answerMyQuestion({ prompt: 'What is the capital of France?' });
+
+for await (const chunk of res.textStream) {
+  console.log(chunk);
+}
